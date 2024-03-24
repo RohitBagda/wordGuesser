9873c1f48bcd03b78b76afe982de9b8cb46b30c6
RohitBagda <rohitbagda@gmail.com>
2024-03-23 21:55:11 -0500

    Initial Commit

---
+++ wordGuesser/wordGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordGuesserApp.swift
+//  wordGuesser
+//
+//  Created by Rohit Bagda on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct wordGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
